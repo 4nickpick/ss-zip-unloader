6ec6b3a1825712724b6b259be0b578a9e260c957
Nicholas Pickering <2nickpick@gmail.com>
2012-12-23 02:21:20 -0500

    alpha release

---
+++ action.php
@@ -0,0 +1,50 @@
+<?php
+
+    if( isset($_POST['verb']) && $_POST['verb'] == 'ss_zip_unloader' )
+    {
+        if ( ! function_exists( 'wp_handle_upload' ) ) require_once( ABSPATH . 'wp-admin/includes/file.php' );
+        $uploadedfile = $_FILES['file'];
+        $upload_overrides = array( 'test_form' => false );
+        $ext = pathinfo($uploadedfile['name'], PATHINFO_EXTENSION);
+        if( $ext == 'zip' )
+        {   
+            if( isset($_POST['extract']) && $_POST['extract'] == 'on' )
+            {
+                $zip = zip_open($uploadedfile['tmp_name']);
+                
+                //var_dump($zip);
+                
+                while($zip_read = zip_read($zip)):
+
+                    $zip_entry_name = zip_entry_name($zip_read);
+                    $uploads_dir = wp_upload_dir();
+                    if(strpos($zip_entry_name, '.')):
+                        $path_to_touch = $uploads_dir['path'] . '/' . $zip_entry_name;
+                        touch($path_to_touch);
+                        $fopen = fopen($path_to_touch, 'w+');
+                        fwrite($fopen, zip_entry_read($zip_read));
+                        fclose($fopen);
+                    else:
+                        @mkdir($uploads_dir['path'] . '/'. $zip_entry_name);
+                    endif;
+
+                endwhile;
+            }
+            
+            $movefile = wp_handle_upload( $uploadedfile, $upload_overrides );
+            if ( isset($movefile['url']) ) {
+                echo '<div id="message" class="updated"><p>Files uploaded successfully. <br />URL: <a href="' . $movefile['url'] . '">' . $movefile['url'] . '</a><br /></p></div>';
+            } else {
+            echo '<div id="message" class="error"><p>File upload failed.</p></div>';
+            }
+        }   
+        else
+        {
+            echo '<div id="message" class="error"><p>Only Zip files are accepted.</p></div>';
+        }
+    }
+    else if( isset($_POST['verb']) )
+    {
+        wp_die("Coming from the wrong angle.");
+    }
+?>
---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+
+
+/*
+    Plugin Name: SS Zip Unloader
+    Author Name: Nicholas Pickering
+    Author Email: 2nickpick@gmail.com
+    Plugin URI: http://soundshrew.com/plugins/ss_zip_unloader
+    Plugin Version: 1.0
+*/
+
+// create menu page in Tools Menu
+
+// create form to upload zip file
+
+// on post, upload file to temp
+
+// extract contents into /uploads
+
+// remove temp file
+
+
+
+function ss_zip_unloader_menu() {
+    add_management_page (
+        __('SS Zip Unloader'),
+        __('SS Zip Unloader'),
+        'manage_options',
+        'ss-zip-unloader',
+        'ss_zip_unloader_callback'        
+    );
+}
+add_action('admin_menu', 'ss_zip_unloader_menu');
+
+function ss_zip_unloader_callback() { ?>
+    
+    <?php screen_icon(); ?> <h2>SS Zip Unloader</h2>
+    <?php include( plugin_dir_path(__FILE__) . 'action.php' ); ?>
+    <form action="" method="post" enctype="multipart/form-data">
+    <input type="hidden" name="verb" id="verb" value="ss_zip_unloader" />
+    <table>
+        <tr>
+            <th>
+                Zip File: 
+            </th>
+            <td>
+                <input type="file" name="file" id="file" /> <input type="checkbox" name="extract" id="extract" checked="checked"><small>Extract zip</small> <input type="submit" value="Upload" />
+            </td>
+        </tr>
+    </table>
+    </form>
+    
+    <hr />
+    <small><a href="http://soundshrew.com/bbq-fund" target="_blank">Soundshrew: Keep the Programmer Fed</a></small>
+    <?php
+}
+?>
